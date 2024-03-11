8e4122f30796d64995b2d026f5ff787551adb05b
Daren-NDIZE <117444801+Daren-NDIZE@users.noreply.github.com>
2024-03-11 13:07:34 +0100

    first commit

---
+++ src/main/java/com/api/backend/Fonds_routier/FondsRoutierApplication.java
@@ -0,0 +1,13 @@
+package com.api.backend.Fonds_routier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondsRoutierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FondsRoutierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/backend/Fonds_routier/FondsRoutierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.backend.Fonds_routier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondsRoutierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
