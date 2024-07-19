c4bcc2457f06b0d27d0e895b2ca68b0f63e7ff25
Daren-NDIZE <117444801+Daren-NDIZE@users.noreply.github.com>
2024-07-19 12:58:30 +0200

    update

--- src/main/java/com/api/backend/Fonds_routier/model/ProjetMINHDU.java
+++ src/main/java/com/api/backend/Fonds_routier/model/ProjetMINHDU.java
@@ -13,6 +13,7 @@ public class ProjetMINHDU extends Projet{
 
     @NotNull
     private String ville;
+    @NotNull
     private String type_travaux;
     @Column(columnDefinition = "TEXT")
     private String troçon;
